0defb547132bbc7b080eb043a14fb772cddc20b3
edilson591 <edilson591@gmail.com>
2026-04-04 11:45:10 -0300

    fixed: error vercel

--- backend/src/interfaces/http/app.ts
+++ backend/src/interfaces/http/app.ts
@@ -11,7 +11,7 @@ const app = express();
 //setando o cors e o json
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:5173"];
+  : ["http://localhost:5174"];
 
 app.use(
   cors({
